ca821a81147383babd3431de1867a66537bc3ea6
村井謙太 <muraikenta0507@gmail.com>
2017-02-06 17:25:14 +0900

    Lesson2 prototype

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get '/about' => 'home#about'
 
+  get 'notes/index' => 'notes#index'
+  get 'notes/:id' => 'notes#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
