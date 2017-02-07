436d13f39b0d80fcdb67675d657cd600d051b2f9
村井謙太 <muraikenta0507@gmail.com>
2017-02-07 11:43:17 +0900

    Lesson3 prototype

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,9 +1,36 @@
 class NotesController < ApplicationController
   def index
-    @notes = Note.all
+    @notes = Note.all.order(created_at: :desc)
   end
 
   def show
     @note = Note.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @note = Note.new(title: params[:title], content: params[:content])
+    @note.save
+    redirect_to "/notes/#{@note.id}"
+  end
+
+  def edit
+    @note = Note.find_by(id: params[:id])
+  end
+
+  def update
+    @note = Note.find_by(id: params[:id])
+    @note.title = params[:title]
+    @note.content = params[:content]
+    @note.save
+    redirect_to "/notes/#{@note.id}"
+  end
+
+  def destroy
+    @note = Note.find_by(id: params[:id])
+    @note.destroy
+    redirect_to '/notes/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get '/about' => 'home#about'
 
   get 'notes/index' => 'notes#index'
+  get 'notes/new' => 'notes#new'
+  get 'notes/:id/edit' => 'notes#edit'
   get 'notes/:id' => 'notes#show'
+  post 'notes' => 'notes#create'
+  post 'notes/:id/update' => 'notes#update'
+  post 'notes/:id/destroy' => 'notes#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
